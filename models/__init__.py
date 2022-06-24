f9a9a113177a9117435f6dd5d85b08f9c2346d98
Diogo Nascimento <diogotnascimento94@gmail.com>
2022-06-24 16:54:11 -0400

    New: Colored Object Contour

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from .caffeDetectorImageTransformation import CaffeDetectorImageTransformation
 from .rotationImageTransformation import RotationImageTransformation
+from .objectContourImageTransormation import ObjectContourImageTransformation
