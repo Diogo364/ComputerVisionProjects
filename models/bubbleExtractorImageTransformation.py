5f925c62e82220dd6f5d1a838c0a7bcca4260e7d
Diogo364 <diogotnascimento94@gmail.com>
2022-07-01 02:05:18 -0300

    Feature: Bubble test answer extractor

---
+++ models/bubbleExtractorImageTransformation.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+import numpy.typing as npt
+from .documentScannerImageTransformation import DocumentScannerImageTransformation
+from .interface import ImageTransformationInterface
+
+
+class BubbleExtractorImageTransformation(ImageTransformationInterface):
+    def __init__(self, answer_options=['a', 'b', 'c', 'd', 'e']):
+        self.__doc_scanner = DocumentScannerImageTransformation()
+        self._question_cnts = []
+        self.__answer_options = answer_options
+        self.answers = {}
+        self.__answer_contours = []
+
+    @staticmethod
+    def __sort_points(pts, left_right=True):
+        order_element = 0 if left_right else 1
+        return sorted(pts, key=lambda x: x.min(axis=0).flatten()[order_element])
+    
+
+    def __set_question_bubbles(self, image: npt.ArrayLike):
+        cnts, _ = cv2.findContours(image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in cnts:
+            x, y, w, h = cv2.boundingRect(cnt)
+            ratio = w / h
+            if w >= 15 and h >= 15 and ratio >= 0.9 and ratio <= 1.5:
+                self._question_cnts.append(cnt)
+
+    @staticmethod
+    def __extract_answer_bubble(image, bubbles):
+        bubbled = -np.inf, 0
+        for letter_idx, bubble_cnt in enumerate(bubbles):
+            mask = np.zeros(image.shape, dtype=np.uint8)
+            cv2.drawContours(mask, [bubble_cnt], -1, 255, -1)
+            mask = cv2.bitwise_and(image, image, mask=mask)
+            total = cv2.countNonZero(mask)
+            if total > bubbled[0]:
+                bubbled = (total, letter_idx)
+        return bubbled
+
+    def __set_answers(self, image, n_choices=5):
+        for question_number, contour_row in enumerate(np.arange(0, len(self._question_cnts), n_choices), start=1):
+            left_right_bubbles = self.__sort_points(self._question_cnts[contour_row:contour_row+n_choices])
+            _, answer_id = self.__extract_answer_bubble(image, left_right_bubbles)
+            self.answers[question_number] = self.__answer_options[answer_id]
+            
+            self.__answer_contours.append(left_right_bubbles[answer_id])
+
+    
+    def __call__(self, image: npt.ArrayLike) -> list[npt.ArrayLike]:
+        smart_cropped_image = self.__doc_scanner(image.copy(), binarization=False)[0]
+        processed_image = cv2.cvtColor(smart_cropped_image, cv2.COLOR_BGR2GRAY)
+        binary_image = cv2.threshold(processed_image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        self.__set_question_bubbles(binary_image)
+        self._question_cnts = self.__sort_points(self._question_cnts, left_right=False)
+        self.__set_answers(binary_image)
+        smart_cropped_image = cv2.drawContours(smart_cropped_image, self.__answer_contours, -1, (0, 255, 0), 3)
+        text = f"Answers: {self.answers}"
+        cv2.putText(smart_cropped_image, text, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 2)
+        return [smart_cropped_image]
